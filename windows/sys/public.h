bb7d48102ea837577268e23ed240dbf01067372c
Michal Maruska <mmaruska@gmail.com>
2024-12-30 17:42:29 +0100

    declare new IOCTL_KBFILTR_SET_FORK

--- windows/sys/public.h
+++ windows/sys/public.h
@@ -8,4 +8,11 @@
                                                         METHOD_BUFFERED,    \
                                                         FILE_READ_DATA)
 
+
+#define IOCTL_KBFILTR_SET_FORK CTL_CODE( FILE_DEVICE_KEYBOARD,   \
+                                         IOCTL_INDEX + 1,        \
+                                         METHOD_BUFFERED,        \
+                                         FILE_READ_DATA | FILE_WRITE_DATA)
+
+
 #endif
