0abfa40e25535d0f72b81fdff97102d8861da011
Michal Maruska <mmaruska@gmail.com>
2024-12-30 17:44:09 +0100

    by duplicating the code - brrr

--- windows/sys/rawpdo.c
+++ windows/sys/rawpdo.c
@@ -85,7 +85,20 @@ Return Value:
     case IOCTL_KBFILTR_GET_KEYBOARD_ATTRIBUTES:
     case IOCTL_KBFILTR_SET_FORK:
         WDF_REQUEST_FORWARD_OPTIONS_INIT(&forwardOptions);
+
+        // so we put it into a different queue!!!!
         status = WdfRequestForwardToParentDeviceIoQueue(Request, pdoData->ParentQueue, &forwardOptions);
+
+        if (!NT_SUCCESS(status)) {
+            WdfRequestComplete(Request, status);
+        }
+        break;
+    case IOCTL_KBFILTR_SET_FORK:
+        WDF_REQUEST_FORWARD_OPTIONS_INIT(&forwardOptions);
+
+        // so we put it into a different queue!!!!
+        status = WdfRequestForwardToParentDeviceIoQueue(Request, pdoData->ParentQueue, &forwardOptions);
+
         if (!NT_SUCCESS(status)) {
             WdfRequestComplete(Request, status);
         }
