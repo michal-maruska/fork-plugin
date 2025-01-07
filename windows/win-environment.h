aa49c0c9c087863641d7fb0a16b60ddb334240c2
Michal Maruska <mmaruska@gmail.com>
2025-01-07 22:51:34 +0100

    detail_of: use the E0 bit (inside the USHORT)

--- windows/win-environment.h
+++ windows/win-environment.h
@@ -102,7 +102,8 @@ public:
 
   USHORT detail_of(const extendedEvent& event) const OVERRIDE{
     // note: we ignore the E0/E1 bits!
-    return event.key;
+    // I might even fork to an E0 key!
+    return event.key | ((event.flags & KEY_E0)?0:1<<8);
   }
 
   bool ignore_event(const extendedEvent& pevent) OVERRIDE{
